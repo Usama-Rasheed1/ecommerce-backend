f91e27119f5c4a1a9a8a114a05913d903b54cafa
Usama Rasheed <usamara760@gmail.com>
2024-09-04 23:43:01 +0500

    Create user schema and setup route

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 
+// Importing Routes
+import userRoute  from './routes/user.js';
+
 const port = 5000;
 const app = express();
 
+// Using Routes
+app.use("/api/v1/user", userRoute);
 
 app.listen(port, ()=>{
     console.log(`Express is working on http://localhost:${port}`);
